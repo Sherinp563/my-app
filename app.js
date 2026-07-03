b91cafe8920e074599576ca6c9eb78f2d4bcba70
sherin <sherisheri563@gmail.com>
2026-07-03 11:53:22 +0530

    update the message for testing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.json({
-        message: 'Hello from Task 22 - Version 3!',
+        message: 'this is from Task 22 - Version 3!',
         version: process.env.APP_VERSION || 'v1',
         timestamp: new Date().toISOString()
     });
